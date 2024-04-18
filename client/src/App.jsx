929ed0f81e98bf512a5a26a20ba897172a30b161
TobyDS <tobydixonsmith@gmail.com>
2024-04-18 17:08:31 +0100

    feat(client): :sparkles: add monaco editor to CodeEditor component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import CodeEditor from './components/CodeEditor';
 
 function App () {
-  const editorHeight = '60vh';
+  const editorHeight = '90vh';
   const language = 'html';
   const templateCode = '<div></div>';
   return (
--- client/src/components/CodeEditor/index.jsx
+++ client/src/components/CodeEditor/index.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
-
-function CodeEditor () {
+import Editor from '@monaco-editor/react';
+import PropTypes from 'prop-types';
+function CodeEditor ({ editorHeight, language, templateCode }) {
   return (
-    <>
-      <div>CodeEditor</div>
-    </>
+    <div className='editor-container'>
+      <Editor
+        height={editorHeight}
+        defaultLanguage={language}
+        defaultValue={templateCode}
+        theme='vs-dark'
+      />
+    </div>
   );
 }
+
+CodeEditor.propTypes = {
+  editorHeight: PropTypes.string.isRequired,
+  language: PropTypes.string.isRequired,
+  templateCode: PropTypes.string.isRequired,
+};
 
 export default CodeEditor;
