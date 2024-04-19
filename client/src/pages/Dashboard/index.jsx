fc72d8598da86152f14f61a0b656de6275b2f470
TobyDS <tobydixonsmith@gmail.com>
2024-04-19 17:18:03 +0100

    feat(client): :sparkles: add dashboard and navbar

---
+++ client/src/pages/Dashboard/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '@components/Navbar';
+import styles from './index.module.css';
+
+function Dashboard () {
+  return (
+    <>
+      <Navbar />
+      <h1>Dashboard</h1>
+    </>
+  );
+}
+
+export default Dashboard;
