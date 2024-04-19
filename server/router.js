fd5ed72178fbebf9ae402f0a433478f24906672e
TobyDS <tobydixonsmith@gmail.com>
2024-04-19 10:53:17 +0100

    feat(server): :truck: rename routes

--- server/router.js
+++ server/router.js
@@ -9,10 +9,10 @@ router.get('/', (req, res) => {
 
 // TODO: REMOVE
 // ! FOR TESTING PURPOSES ONLY:
-router.get('/testing/images/:id', imageController.getImageByIdAsString);
-router.post('/testing/images', imageController.addImage);
+router.get('/testing/image/:id', imageController.getImageByIdAsString);
+router.post('/testing/image', imageController.addImage);
 
-router.get('/images/:id', imageController.getImageById);
+router.get('/image/:id', imageController.getImageById);
 router.get('/images', imageController.getImages);
 
 router.all('*', (req, res) => {
