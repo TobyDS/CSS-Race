66581d84f944b1d23328881bd930963cf4c67c4b
TobyDS <tobydixonsmith@gmail.com>
2024-04-19 16:40:31 +0100

    feat(client): :package: add material ui

--- client/src/components/RenderFrame/index.jsx
+++ client/src/components/RenderFrame/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import DOMPurify from 'dompurify';
 import PropTypes from 'prop-types';
 import styles from './index.module.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function RenderFrame ({ combinedCode }) {
   const sanitizedCode = DOMPurify.sanitize(combinedCode);
