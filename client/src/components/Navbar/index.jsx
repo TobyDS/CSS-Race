8e758eddfca249adbf9fc7d14f9bec1da135b0d0
TobyDS <tobydixonsmith@gmail.com>
2024-04-20 16:53:17 +0100

    feat(client): :sparkles: add logo and redirect to homepage

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+import styles from './index.module.css';
 
 function Navbar () {
   return (
     <AppBar position='static'>
       <Toolbar>
-        <Typography
-          variant='h6'
-          noWrap
-          component='a'
-          href='#app-bar-with-responsive-menu'
-          sx={{
-            mr: 2,
-            display: { xs: 'none', md: 'flex' },
-            fontFamily: 'monospace',
-            fontWeight: 700,
-            letterSpacing: '.1rem',
-            color: 'inherit',
-            textDecoration: 'none',
-          }}
-        >
-          CSS Race
-        </Typography>
+        <Link to='/' className={styles.Logo}>
+          <img
+            alt='Logo'
+            src='/src/assets/images/CSS Logo Dark Mode.svg'
+            height='30px'
+          />
+        </Link>
       </Toolbar>
     </AppBar>
   );
