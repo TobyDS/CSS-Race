a989858fe3e71e429a006104e2b1054e2647387a
TobyDS <tobydixonsmith@gmail.com>
2024-04-21 01:43:52 +0100

    feat(server): :sparkles: get image for room on connect

--- server/controllers/socket.js
+++ server/controllers/socket.js
@@ -1,16 +1,33 @@
 const generateRoomId = require('../utils/generateRoomId');
+const Image = require('../models/image');
+
+// Mapping of room IDs to images
+const roomImages = {};
 
 module.exports = function (io) {
   io.on('connection', (socket) => {
     console.log('a user connected');
 
-    // Generate a new room ID when a user connects without a room ID
-    socket.on('create_room', () => {
+    // Generate a new room ID and a random image when a user connects without a room ID
+    socket.on('create_room', async () => {
       try {
         const roomId = generateRoomId();
-        socket.emit('room_id', roomId);
-        socket.join(roomId);
-        console.log(`Room ${roomId} created`);
+        Image.random((image) => {
+          const base64Image = image.img.toString('base64');
+          const base64Image2x = image.img_2x.toString('base64');
+          const parsedImage = {
+            id: image._id,
+            img: base64Image,
+            img_2x: base64Image2x,
+            colors: image.colors,
+          };
+          console.log('image', parsedImage);
+          roomImages[roomId] = parsedImage;
+          socket.emit('room_id', roomId);
+          socket.emit('image', parsedImage);
+          socket.join(roomId);
+          console.log(`Room ${roomId} created`);
+        });
       } catch (error) {
         console.error(`Error creating room: ${error}`);
       }
@@ -19,9 +36,15 @@ module.exports = function (io) {
     // Join a room when a user connects with a room ID
     socket.on('join_room', (roomId) => {
       try {
-        socket.join(roomId);
-        console.log(`User joined room ${roomId}`);
-        io.to(roomId).emit('message', `A new user has joined room ${roomId}`);
+        const image = roomImages[roomId];
+        if (image) {
+          socket.emit('image', image);
+          socket.join(roomId);
+          console.log(`User joined room ${roomId}`);
+          io.to(roomId).emit('message', `A new user has joined room ${roomId}`);
+        } else {
+          console.error(`No image found for room ${roomId}`);
+        }
       } catch (error) {
         console.error(`Error joining room: ${error}`);
       }
