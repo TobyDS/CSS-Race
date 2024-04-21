e18af1e753e79673dea517dec5914ea8d6413bcd
TobyDS <tobydixonsmith@gmail.com>
2024-04-21 01:09:12 +0100

    feat(server): :goal_net: add error catching

--- server/controllers/socket.js
+++ server/controllers/socket.js
@@ -6,17 +6,25 @@ module.exports = function (io) {
 
     // Generate a new room ID when a user connects without a room ID
     socket.on('create_room', () => {
-      const roomId = generateRoomId();
-      socket.emit('room_id', roomId);
-      socket.join(roomId);
-      console.log(`Room ${roomId} created`);
+      try {
+        const roomId = generateRoomId();
+        socket.emit('room_id', roomId);
+        socket.join(roomId);
+        console.log(`Room ${roomId} created`);
+      } catch (error) {
+        console.error(`Error creating room: ${error}`);
+      }
     });
 
     // Join a room when a user connects with a room ID
     socket.on('join_room', (roomId) => {
-      socket.join(roomId);
-      console.log(`User joined room ${roomId}`);
-      io.to(roomId).emit('message', `A new user has joined room ${roomId}`);
+      try {
+        socket.join(roomId);
+        console.log(`User joined room ${roomId}`);
+        io.to(roomId).emit('message', `A new user has joined room ${roomId}`);
+      } catch (error) {
+        console.error(`Error joining room: ${error}`);
+      }
     });
 
     socket.on('disconnect', () => {
