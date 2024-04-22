96ab1850cd89961be3b390038634e9e531680d89
TobyDS <tobydixonsmith@gmail.com>
2024-04-22 22:59:35 +0100

    feat(client): :sparkles: add styling on battle page

--- client/src/components/RenderFrame/index.jsx
+++ client/src/components/RenderFrame/index.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import DOMPurify from 'dompurify';
 import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+
 import styles from './index.module.css';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -10,13 +13,21 @@ import '@fontsource/roboto/700.css';
 function RenderFrame ({ combinedCode }) {
   const sanitizedCode = DOMPurify.sanitize(combinedCode);
   return (
-    <iframe
-      className={styles.frame}
-      srcDoc={sanitizedCode}
-      title='Render Frame'
-      width='400'
-      height='300'
-    ></iframe>
+    <div className={styles.flexContainer}>
+      <div className={styles.renderFrameContainer}>
+        <div className={styles.flexRow}>
+          <p>Your Code</p>
+        </div>
+        <iframe
+          className={styles.frame}
+          srcDoc={sanitizedCode}
+          title='Render Frame'
+          width='400'
+          height='300'
+        ></iframe>
+      </div>
+      <p>Match 0%</p>
+    </div>
   );
 }
 
--- client/src/components/RenderImage/index.jsx
+++ client/src/components/RenderImage/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
+import renderStyles from '../RenderFrame/index.module.css';
+import styles from './index.module.css';
+
 function RenderImage ({ image }) {
   if (!image || !image.img) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <img src={`data:image/jpeg;base64,${image.img}`} />
+    <div className={styles.flexContainer}>
+      <div className={renderStyles.renderFrameContainer}>
+        <div className={renderStyles.flexRow}>
+          <p>Target</p>
+          <p>400px x 300px</p>
+        </div>
+        <img src={`data:image/jpeg;base64,${image.img}`} />
+      </div>
     </div>
   );
 }
