47fcaa8e6240c708ea8136029145cc2086f1c22c
TobyDS <tobydixonsmith@gmail.com>
2024-04-23 15:49:26 +0100

    feat(client): :wheelchair: add tooltip to copy in room

--- client/src/components/CopyClipboardButton/index.jsx
+++ client/src/components/CopyClipboardButton/index.jsx
@@ -1,24 +1,50 @@
-import { IconButton, Typography } from '@mui/material';
+import { IconButton, Tooltip, Typography } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 function CopyClipboardButton ({ roomId }) {
+  const [tooltipText, setTooltipText] = useState('Copy');
+  const [open, setOpen] = useState(false);
+
   async function handleCopy () {
     try {
       await navigator.clipboard.writeText(roomId);
+      setTooltipText('Copied');
+      handleTooltipOpen();
+      setTimeout(() => setTooltipText('Copy'), 2000); // Reset tooltip text after 2 seconds
     } catch (err) {
       console.error('Failed to copy room ID: ', err);
     }
   }
 
+  function handleTooltipClose () {
+    setOpen(false);
+  }
+
+  function handleTooltipOpen () {
+    setOpen(true);
+  }
+
   return (
     <>
       <Typography variant='h5' color='text.primary'>
+        Room ID:
+      </Typography>
+      <Typography variant='h5' color='text.primary'>
         {roomId}
       </Typography>
-      <IconButton aria-label='copy' color='primary' onClick={handleCopy}>
-        <ContentCopyIcon />
-      </IconButton>
+      <Tooltip
+        open={open}
+        onClose={handleTooltipClose}
+        onOpen={handleTooltipOpen}
+        title={tooltipText}
+        placement='top'
+      >
+        <IconButton aria-label='copy' color='primary' onClick={handleCopy}>
+          <ContentCopyIcon />
+        </IconButton>
+      </Tooltip>
     </>
   );
 }
