4318d089db3db29c46ec67fba309883112e29261
TobyDS <tobydixonsmith@gmail.com>
2024-04-23 15:58:04 +0100

    feat(client): :wheelchair: add tooltips to colour chips

--- client/src/components/ClickableChips/index.jsx
+++ client/src/components/ClickableChips/index.jsx
@@ -1,29 +1,55 @@
 import PropTypes from 'prop-types';
-import { Avatar, Chip } from '@mui/material';
+import { Avatar, Chip, Tooltip } from '@mui/material';
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { useState } from 'react';
 
 function ClickableChips ({ colors }) {
+  const [tooltipText, setTooltipText] = useState({});
+  const [open, setOpen] = useState({});
+
   async function handleClick (color) {
     try {
-      await navigator.clipboard.writeText(color);
+      await navigator.clipboard.writeText(color.slice(1)); // Remove the # from the color
+      setTooltipText({ ...tooltipText, [color]: 'Copied' });
+      handleTooltipOpen(color);
+      setTimeout(
+        () => setTooltipText({ ...tooltipText, [color]: 'Copy' }),
+        2000
+      ); // Reset tooltip text after 2 seconds
     } catch (err) {
-      console.error('Failed to copy room ID: ', err);
+      console.error('Failed to copy color: ', err);
     }
   }
+
+  function handleTooltipClose (color) {
+    setOpen({ ...open, [color]: false });
+  }
+
+  function handleTooltipOpen (color) {
+    setOpen({ ...open, [color]: true });
+  }
+
   return (
     <div>
       {colors.map((color) => (
-        <Chip
-          avatar={
-            <Avatar sx={{ bgcolor: color }}>
-              <ContentCopyIcon fontSize='small' />
-            </Avatar>
-          }
+        <Tooltip
+          open={open[color]}
+          onClose={() => handleTooltipClose(color)}
+          onOpen={() => handleTooltipOpen(color)}
+          title={tooltipText[color] || 'Copy'}
           key={color}
-          label={color}
-          onClick={() => handleClick(color)}
-          style={{ margin: '0.5rem' }}
-        />
+        >
+          <Chip
+            avatar={
+              <Avatar sx={{ bgcolor: color }}>
+                <ContentCopyIcon fontSize='small' />
+              </Avatar>
+            }
+            label={color}
+            onClick={() => handleClick(color)}
+            style={{ margin: '0.5rem' }}
+          />
+        </Tooltip>
       ))}
     </div>
   );
