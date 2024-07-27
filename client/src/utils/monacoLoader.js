59b0e659d8683e2ed09fb9434cbe8fd5bb50a052
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-07-27 19:58:31 +0100

    fix(client): use cdn for monaco-editor in production

--- client/src/utils/monacoLoader.js
+++ client/src/utils/monacoLoader.js
@@ -3,7 +3,9 @@ import loader from '@monaco-editor/loader';
 import { emmetHTML, emmetCSS } from 'emmet-monaco-es';
 
 // Configure the loader to use the locally installed Monaco Editor
-loader.config({ paths: { vs: '/node_modules/monaco-editor/min/vs' } });
+if (import.meta.env.MODE === 'development') {
+  loader.config({ paths: { vs: '/node_modules/monaco-editor/min/vs' } });
+}
 
 export function initMonaco () {
   return loader.init().then((monacoInstance) => {
