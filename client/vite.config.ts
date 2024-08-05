295992c2a110049d3c38608ebbf186bdc1d30220
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-08-05 16:35:29 +0100

    test(client): update test setup

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['src/__tests__/setup.js'],
+    setupFiles: ['src/__tests__/setup.ts'],
   },
   resolve: {
     alias: {
