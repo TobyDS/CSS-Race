2af83155d1a256b8ee7ed2b17891e73b90991fcb
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-08-06 21:29:44 +0100

    feat(client): convert userStatusUtils to typescript

--- client/src/utils/userStatusUtils.tsx
+++ client/src/utils/userStatusUtils.tsx
@@ -4,8 +4,13 @@ import userOutlineRed from '@images/user-outline-red.svg';
 import userSolidBlue from '@images/user-solid-blue.svg';
 import userSolidRed from '@images/user-solid-red.svg';
 
+interface getSvgProps {
+  playerNum: 1 | 2;
+  targetUserReady: boolean | undefined;
+}
+
 export const userStatusUtils = {
-  getSVG: function ({ playerNum, targetUserReady }) {
+  getSVG: function ({ playerNum, targetUserReady }: getSvgProps) {
     let imgSrc = userOutlineGrey;
     let altText = 'user not connected';
 
@@ -20,7 +25,7 @@ export const userStatusUtils = {
 
     return <img src={imgSrc} alt={altText} />;
   },
-  getUserStatusText: function (targetUserReady) {
+  getUserStatusText: function (targetUserReady: boolean | undefined) {
     switch (targetUserReady) {
     case false:
       return 'Not Ready';
